2ba949a3f5d9b9615d48151b80720ded015e6a10
Paolo Valdez <nicobusish@gmail.com>
2026-02-02 19:18:26 -0700

    Entrada y salida de datos Python 2/2/2026

---
+++ Temario Concurso 2026/1_PythonEntradaDeDatos.py
@@ -0,0 +1,9 @@
+"""Cetis107 Febrero 2026 - Paolo Nicholas Valdez Bernal 🦇"""
+
+"""Entrada de datos"""
+variable = input()
+
+"""Entrada de datos numericos"""
+variable_numerica = int(input())
+
+"""https://github.com/nicho808/Codigos-Concurso-De-Ciencias-TEC-2026"""
---
+++ Temario Concurso 2026/2_PythonSalidaDeDatos.py
@@ -0,0 +1,21 @@
+"""Cetis107 Febrero 2026 - Paolo Nicholas Valdez Bernal 🦇"""
+
+"""Imprimir caracteres"""
+print("Hola Mundo!")
+
+"""Imprimir variable"""
+variable_1 = 5
+print(variable_1)
+
+"""Imprimir calculo"""
+variable_2 = 10
+print(variable_1 + variable_2)
+
+"""Imprimir varios elementos"""
+print("Cinco por diez es igual a", variable_1 * variable_2)
+
+"""Imprimir en la misma linea"""
+print("Este texto se imprimira en la ", end="")
+print("misma linea que el anterior!")
+
+"""https://github.com/nicho808/Codigos-Concurso-De-Ciencias-TEC-2026"""
